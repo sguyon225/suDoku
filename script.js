608352c8f49feb74068f4d370a1cddf7ae54c7f1
Sean Guyon <sguyon225@gmail.com>
2025-01-16 08:22:46 -0500

    maybe makes board

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-function changeWord(word) {
-    if (window.localStorage) {
-        if (word) {
-            localStorage.setItem('newWord', word);
-        }
+// function makeBoxes(word) {
+//     if (window.localStorage) {
+//         if (word) {
+//             localStorage.setItem('newWord', word);
+//         }
 
-        if (localStorage.getItem('newWord')) {
-            document.getElementById('displayText').textContent = localStorage.getItem('newWord');
-        }
+//         if (localStorage.getItem('newWord')) {
+//             document.getElementById('displayText').textContent = localStorage.getItem('newWord');
+//         }
+//     }
+// }
+
+// function clearWord() {
+//     localStorage.removeItem('newWord');
+//     document.getElementById('displayText').textContent = "Your text will appear here.";
+// }
+
+function makeBoxes(){
+    for(i=0;i<9;i++){
+        box=document.createElement('label').classList("box");
+        document.getElementById("board").appendChild(box);
     }
 }
-
-function clearWord() {
-    localStorage.removeItem('newWord');
-    document.getElementById('displayText').textContent = "Your text will appear here.";
+function clearboxes(){
+    board=document.getElementById("board");
+    while(board.firstChild()){
+        board.removeChild(board.lastChild())
+    }
 }
