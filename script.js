6a30fa91743031de7cda15b1411c93666d642e98
Sean Guyon <sguyon225@gmail.com>
2025-01-21 16:27:41 -0500

    good boxes

--- script.js
+++ script.js
@@ -9,9 +9,47 @@ function makeBoxes() {
 
 function clearBoxes() {
     let board = document.getElementById("board");
-    while (board.firstChild) {
-        board.removeChild(board.firstChild);
-    }
+    let boxes = board.getElementsByClassName("box");
+
+    Array.from(boxes).forEach(box => {
+        box.classList.remove("blue", "green");
+    });
+}
+
+function makeAffils() {
+    let game = document.getElementById("game");
+    let choices = [];
+    console.log(choices);
+
+    fetch("affiliations.txt")
+        .then((res) => res.text())
+        .then((text) => {
+            choices.push(...text.split("\n"));
+
+            // Ensure choices are populated before continuing
+            for (let i = 1; i <= 6; i++) {
+                let item = Math.floor(Math.random() * choices.length);
+                let affil = document.createElement("div");
+                let span = document.createElement("span");
+                let textNode = document.createTextNode(choices[item]);
+                span.appendChild(textNode);
+                affil.appendChild(span);
+                affil.classList.add("affiliation");
+                affil.setAttribute("id", "a" + i);
+                game.appendChild(affil);
+
+                // Remove the used choice
+                choices.splice(item, 1);
+            }
+        })
+        .catch((e) => console.error(e));
+}
+
+
+
+function makeGame(){
+    makeAffils()
+    makeBoxes()
 }
 
 function changeColor(box) {
